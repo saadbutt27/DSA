fa86058f8a52997d042d3091518dfd63169023cc
Saad Nisar Butt <83548173+saadbutt27@users.noreply.github.com>
2024-09-17 11:52:54 +0500

    Solve pair sum and majorith element - leetcode (using moore's voting algorithm)

--- array/vectors.cpp
+++ array/vectors.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 #include "vector"
+#include "algorithm"
 
 int singleNumber(vector<int>& nums) {
     // Leetcode problem # 136. Single Number
@@ -84,8 +85,109 @@ int maxSubarraySum(vector<int> nums) {
     return maxSum;
 }
 
+vector<int> pairSum(vector<int> nums, int target) {
+    vector<int> ans;
+
+    // BRUTE FORCE APPROACH - O(n^2)
+    // for (int i=0; i<nums.size(); i++) {
+    //     for (int j=i+1; j<nums.size(); j++) {
+    //         if (nums[i] + nums[j] == target) {
+    //             ans.push_back(i);
+    //             ans.push_back(j);
+    //             return ans;
+    //         }
+    //     }
+    // }
+
+    // OPTIMIZED APPROACH - O(n)
+    // Since the array in parameter is sorted, so we can think of an optimized approach.
+    // If we sum starting elements, the sum will be lesser, and if we sum ending elements, the summ will be bigger.
+    // We can use two-pointer approach, one at start and one at end.
+    // We will sum start and end element iteratively
+    // 1. If the sum is > target, we have to reduce it, and we can do it by moving to the next lesser element which is
+    // just before the ending element.
+    // 2. If the sum is < target, we have to increase it, and we can do it by moving to the next bigger element which is
+    // just after the starting element.
+    // 3. If the sum == target, then we have found, we can return the indices.
+
+    int start = 0, end = nums.size() - 1;
+    while (start < end) {
+        if (nums[start] + nums[end] > target) {
+            end--;
+        } else if (nums[start] + nums[end] < target) {
+            start++;
+        } else {
+            ans.push_back(start);
+            ans.push_back(end);
+            return ans;
+        }
+    }
+
+    return ans;
+}
+
+int majorityElement(vector<int> nums) {
+    // Majority elements exists floor n/2 times in the array.
+    int n = nums.size();
+    // BRUTE FORCE APPROACH - O(n^2)
+    // for (int val: nums) {
+    //     int freq = 0;
+    //     for (int el: nums) {
+    //         if (val == el) {
+    //             freq++;
+    //         }
+    //     }
+    //     if (freq > n/2) {
+    //         return val;
+    //     }
+    // }
+
+    // SLIGHTLY OPTIMIZED APPROACH - O(nlgn + n) = O(nlgn)
+    // sort(nums.begin(), nums.end()); // O(nlogn)
+    // int freq = 1, ans = nums[0]; 
+    // for (int i=1; i<n; i++) {
+    //     if (nums[i] == nums[i-1]) {
+    //         freq++;
+    //     } else {
+    //         freq = 1;
+    //         ans = nums[i];
+    //     }
+    //     if (freq > n/2) {
+    //         return ans;
+    //     }
+    // }
+
+    // MOORE'S VOTING ALGORITHM - O(n)
+    // Intuition: If there's a majority element whose frequency is > n/2, then if we start tracking frequency for all 
+    // elements from the start, then the majoity element's frquency will be greater always.
+    // here the frequency is equivalent to power, the frequency of majorith element will be that much that if we 
+    // decrease it, at the end, the power will be grater for the majority element.
+    int freq = 0, ans = nums[0]; 
+    for (int i=0; i<n; i++) {
+        if (freq == 0) {
+            ans = nums[i];
+        }
+
+        if (ans == nums[i]) {
+            freq++;
+        } else {
+            freq--;
+        }
+    }
+
+    int count = 0;
+    for (int val: nums) {
+        if (val == ans) {
+            count++;
+        }
+    }
+
+    if (count > n/2) return ans;
+    else return -1;
+}
+
 int main() {
-    vector<int> nums = {3, -4, 5, 4, -1, 7, -8};
+    vector<int> nums = {1, 2, 3};
 
     // cout << singleNumber(nums) << endl;
     // cout << linearSearch(nums, 2) << endl;
@@ -102,6 +204,11 @@ int main() {
     // }
     // cout << endl;
 
-    cout << maxSubarraySum(nums) << endl;
+    // cout << maxSubarraySum(nums) << endl;
+    // vector<int> ans = pairSum(nums, 26);
+    // cout << ans[0] << ", " << ans[1] <<  endl;
+
+    cout << majorityElement(nums) << endl;
+
     return 0;
 }
