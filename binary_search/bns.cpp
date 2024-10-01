a90611a7edaecfe35b9fa93b5ad05bf98414aef9
Saad Nisar Butt <83548173+saadbutt27@users.noreply.github.com>
2024-10-01 12:29:04 +0500

    SOlve 33. Search in a rotated sorted array problem, using binary search technique

--- binary_search/bns.cpp
+++ binary_search/bns.cpp
@@ -40,15 +40,58 @@ int recursiveBinarySearch(vector<int>& arr, int start, int end, int target) {
     return -1;
 }
 
+int search(vector<int>& nums, int target) {
+    // TC = O(logn)
+    // SC = O(1)
+
+    // For a rotated sorted array, either our left half will be sorted, or the right half but not both.
+    // Hence we can use binary search on the sorted part of the array.
+    // For left half, we can check if A[st] <= target <= A[mid-1], is true, we can search left, else right.
+    // For right half, we can check if A[mid+1] <= target <= A[end], is true, we can search right, else left.
+    // For a rotated sorted array, howcan we check if the left half is sorted or the right half?
+    // We can check if A[st] < A[mid], the left half is sorted, else right half is sorted.
+
+    // Modified form of Binary search
+
+    int start = 0, end = nums.size()-1;
+
+    while (start <= end) {
+        int mid = start + (end - start) / 2;
+
+        if(nums[mid] == target) {
+            return mid;
+        }
+
+        if (nums[start] <= nums[mid]) {
+            // Left half is sorted
+            if (nums[start] <= target && target <= nums[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        } else {
+            // Right half is sorted
+            if (nums[mid] <= target && target <= nums[end]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+    }
+
+    return -1;
+}
+
 int main() {
 
-    vector<int> arr = {15, 24, 84, 100, 123};
-    cout << binarySearch(arr, 2) << endl;
-    cout << recursiveBinarySearch(arr, 0, arr.size()-1, 100) << endl;
-    cout << recursiveBinarySearch(arr, 0, arr.size()-1, 84) << endl;
-    cout << binarySearch(arr, 15) << endl;
+    // vector<int> arr = {15, 24, 84, 100, 123};
+    // cout << binarySearch(arr, 2) << endl;
+    // cout << recursiveBinarySearch(arr, 0, arr.size()-1, 100) << endl;
+    // cout << recursiveBinarySearch(arr, 0, arr.size()-1, 84) << endl;
+    // cout << binarySearch(arr, 15) << endl;
 
+    vector<int> nums = {3, 4, 5, 6, 7, 0, 1, 2};
+    cout << search(nums, 0) << endl;
 
-    
     return 0;
 }
