32ada63dd57c7bb79f9adbc0a64caa874dfd0c97
Saad Nisar Butt <83548173+saadbutt27@users.noreply.github.com>
2024-10-08 15:01:10 +0500

    Solved book allocation using binary search

--- binary_search/bns.cpp
+++ binary_search/bns.cpp
@@ -137,6 +137,72 @@ int singleNonDuplicate(vector<int>& nums) {
     return -1; 
 }
 
+bool isValid (vector<int>arr, int n, int m, int maxAllowedPages) {
+    int std = 1, pages = 0;
+    // O(n)
+    for (int i=0; i<n; i++) {
+
+        if (arr[i] > maxAllowedPages) return false;
+
+        if (pages + arr[i] <= maxAllowedPages) {
+            pages += arr[i];
+        } else {
+            std++;
+            pages = arr[i];
+        }
+    }
+
+    return std <= m ? true : false;
+}
+
+int allocateBooks (vector<int>& pages, int m) {
+    // n books, each has A[i] number of pages
+    // allocate books to m number of students so that the max number of pages allocated to a student is aminimum
+    // each book should be allocated to a student.
+    // each student should be allocated at least one book.
+    // allotment should be in a contiguous(sequential) order.
+
+    // find min possible max pages
+    // we can find min an max pages to a student
+    // for pages = [2, 1, 3, 4]
+    // min = 0 , max = sum[all_pages], we've to find our answer in the range of min and max
+    // 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 :: fo pages array
+    // now we can apply binary search on this range
+    // if we find mid of this ranges, it is 5
+    // how do we know that 5 is  avalid or invalid allotment
+    // here 5 is invalid because we are obliged to allocate pages in a contiguous manner and 2, and 3 are not contiguous in the array.
+    // so now we conclude that the max allotment will be > 5, so we'll move to right
+    // if we find  a valid answer which is max, but there is a possibility we can find min of max
+    // hence if we find a valid answer (which fullfills all conditions), we will move to left
+
+    // TC = O(n * logN)
+    int n = pages.size();
+    if (m > n) return -1;
+
+    int sum = 0;
+
+    for (int i=0; i<n; i++) { // O(n)
+        sum += pages[i];
+    }
+
+    int start = 0, end = sum, ans = -1;
+
+    while (start <= end) {  // O(log(range)) => O(logN)
+        int mid = start + (end - start) / 2;
+
+        if (isValid(pages, n, m, mid)) {
+            // Move left, find another min
+            ans = mid;
+            end = mid - 1;
+        } else {
+            // Move right, find max
+            start = mid + 1;
+        }
+    }
+
+    return ans;
+}
+
 int main() {
 
     // vector<int> arr = {15, 24, 84, 100, 123};
@@ -151,8 +217,11 @@ int main() {
     // vector<int> nums = {0, 3, 8, 9, 5, 2};
     // cout << peakIndexInMountainArray(nums) << endl;
 
-    vector<int> nums = {1, 1, 2, 3, 3, 4, 4, 8, 8};
-    cout << singleNonDuplicate(nums) << endl;
+    // vector<int> nums = {1, 1, 2, 3, 3, 4, 4, 8, 8};
+    // cout << singleNonDuplicate(nums) << endl;
+
+    vector<int> pages = {15, 17, 20};
+    cout << allocateBooks(pages, 2) << endl;
 
     return 0;
 }
