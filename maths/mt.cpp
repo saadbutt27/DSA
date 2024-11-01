35dac37737a810cbbaf8780f5cd5a349fd75d4cd
Saad Nisar Butt <83548173+saadbutt27@users.noreply.github.com>
2024-11-01 11:12:47 +0500

    GCD/HCF, Crute force, Optimized approach -> Euclid's Algorithm(Iterative and Recursive)

--- maths/mt.cpp
+++ maths/mt.cpp
@@ -60,6 +60,40 @@ bool isArmstrong(int n) {
     return sum_of_cubes == n_copy;
 }
 
+int gcd(int a, int b) {
+    if (a == 0) return b;
+    if (b == 0) return a;
+    if (a == b) return a;
+
+    int gcd = 1;
+
+    // BRUTE FORCE APPROACH - TC = O(min(a,b))
+    for (int i=1; i<min(a,b); i++) {
+        if (a % i == 0 && b % i == 0)
+            gcd = i;
+    }
+
+    return gcd;
+}
+
+int gcdEuclidIter(int a, int b) {
+    while (a > 0 && b > 0) {
+        if (a > b)
+            a %= b;
+        else    
+            b %= a;
+    }
+
+    if (a == 0) return b;
+    return a;
+}
+
+int gcdEuclidRec(int a, int b) {
+    // Assuming a > b
+    if (b == 0) return a;
+
+    return gcdEuclidRec(b, a % b);
+}
 int main() {
     // int n = 47;
     // cout << n << " is " << isPrime(n) << endl;
@@ -72,8 +106,13 @@ int main() {
 
     // cout << (int)(log10(n) + 1) << endl;
 
-    int n = 153;
-    cout << "Is " << n << " an Armstrong number?\n"<< (isArmstrong(n) ? "Yes" : "No") << endl;
+    // int n = 153;
+    // cout << "Is " << n << " an Armstrong number?\n"<< (isArmstrong(n) ? "Yes" : "No") << endl;
+
+    int a = 20, b = 28;
+    cout << "GCD of " << a << " and " << b << " = " << gcd(a, b) << endl;
+    cout << "GCD (Euclid's Algorithm Iterative) of " << a << " and " << b << " = " << gcdEuclidIter(a, b) << endl;
+    cout << "GCD (Euclid's Algorithm Recursive) of " << a << " and " << b << " = " << gcdEuclidRec(a, b) << endl;
 
     return 0;
 }
