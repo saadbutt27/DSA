208ee1a9ac4ef65b0fec8f549f1f4290baf2b60b
Saad Nisar Butt <83548173+saadbutt27@users.noreply.github.com>
2024-11-01 10:25:06 +0500

    Play with digits in a number

--- maths/mt.cpp
+++ maths/mt.cpp
@@ -26,12 +26,36 @@ int countPrimes(int n) {
     return count;
 }
 
+void printDigits(int n) {
+    int count = 0;
+    int sum = 0;
+
+    // TC = O(log10(n)) =-> 10 is base because we are dividing the number by 10
+    while (n != 0) {
+        int dig = n % 10;
+        cout << dig << " ";
+        count++;
+        sum += dig;
+
+        n /= 10;
+    }
+    cout << endl;
+
+    cout << "Total digits = " << count << endl;
+    cout << "Sum of digits = " << sum << endl;
+}
+
 int main() {
     // int n = 47;
     // cout << n << " is " << isPrime(n) << endl;
 
-    int n = 50;
-    cout << "Count of primes form 1 to " << n << " is " << countPrimes(n) << endl;
+    // int n = 50;
+    // cout << "Count of primes form 1 to " << n << " is " << countPrimes(n) << endl;
+
+    int n = 285452;
+    printDigits(n);
+
+    cout << (int)(log10(n) + 1) << endl;
 
     return 0;
 }
