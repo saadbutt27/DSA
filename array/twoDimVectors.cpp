dec6988bb29693186960127d7de70dac49fd63a8
Saad Nisar Butt <83548173+saadbutt27@users.noreply.github.com>
2024-12-10 23:52:27 +0500

    Solved spiral order problem of 2d arrays

--- array/twoDimVectors.cpp
+++ array/twoDimVectors.cpp
@@ -63,20 +63,69 @@ bool searchMatrix2(vector<vector<int>>& matrix, int target) {
     return false;   
 }
 
-int main() {
-    // vector<vector<int>> mat = {{1,3,5,7},{10,11,16,20},{23,30,34,60}};
-    vector<vector<int>> mat = {{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}};
+vector<int> spiralOrder(vector<vector<int>>& mat) {
+    // TC = O(m * n)
+    int m = mat.size(), n = mat[0].size();
+    int srow = 0, scol = 0, erow=m-1, ecol = n-1;
+    vector<int> ans;
 
-    // rows => mat.size();
-    // cols => mat[i].size();
+    while(srow <= erow && scol <= ecol) {
+        // top 
+        for (int j=scol; j<=ecol; j++) {
+            ans.push_back(mat[srow][j]);
+        }
+
+        // right
+        for (int i=srow+1; i<=erow; i++) {
+            ans.push_back(mat[i][ecol]);
+        }
+
+        // bottom
+        for (int j=ecol-1; j>=scol; j--) {
+            if(srow == erow) break;
+            ans.push_back(mat[erow][j]);
+        }
+
+        // left
+        for (int i=erow-1; i>=srow+1; i--) {
+            if(scol == ecol) break;
+            ans.push_back(mat[i][scol]);
+        }
+
+        srow++; erow--; scol++; ecol--;
+    }
+
+    return ans;
+}
+
+void print1DVector(vector<int> vec) {
+    for (int i=0; i<vec.size(); i++) {
+        cout << vec[i] << " "; 
+    }
+    cout << endl;
+}
+void print2DVector(vector<vector<int>> mat) {
     for (int i=0; i<mat.size(); i++) {
         for (int j=0; j<mat[i].size(); j++) {
             cout << mat[i][j] << " "; 
         }
         cout << endl;
     }
+}
+
+int main() {
+    // vector<vector<int>> mat = {{1,3,5,7},{10,11,16,20},{23,30,34,60}};
+    vector<vector<int>> mat = {{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}};
+
+    // rows => mat.size();
+    // cols => mat[i].size();
+    print2DVector(mat);
 
     // cout << searchMatrix(mat, 34+1) << endl;
-    cout << searchMatrix2(mat, 5) << endl;
+    // cout << searchMatrix2(mat, 5) << endl;
+
+    vector<int> res = spiralOrder(mat);
+    print1DVector(res);
+
     return 0;
 }
